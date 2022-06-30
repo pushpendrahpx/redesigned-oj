53b6e883e566f6ad035630e4c47c8fc8577b1d3f
pushpendrahpx <pushpendra.hpx2001@gmail.com>
2022-06-30 21:56:24 +0530

    Problems Create API Implemented

--- problem/models.py
+++ problem/models.py
@@ -3,6 +3,7 @@ from pyexpat import model
 from random import choice, choices
 from statistics import mode
 from django.db import models
+from pkg_resources import require
 
 # Create your models here.
 
@@ -19,10 +20,13 @@ class Problem(models.Model):
         HARD='HARD'
         NOTDEFINED='NOTDEFINED'
 
+    problemcode = models.CharField(max_length=20, null=False, unique=True, default='')
+    title = models.CharField(max_length=100, null=False, default='', unique=True)
     description = models.TextField(null=False)
     difficulty = models.CharField(choices=DifficultyLevel.choices, null=False, max_length=20)
     score = models.IntegerField(null=False)
-    
+    correctoutput = models.TextField(null=False, default='')
+
 
 class ProblemsHavingTags(models.Model):
     problem = models.ForeignKey(Problem, on_delete=models.CASCADE)
--- problem/views.py
+++ problem/views.py
@@ -1,7 +1,38 @@
+import json
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+
+from .models import Problem
+
 
 def create_problem(request):
-    
-    return HttpResponse('create problems')
+    try:
+        if(request.method != "POST"):
+            return JsonResponse({'status':'Invalid create_problem Method'}, status=400)
+        
+
+        jsonData = json.loads(request.body)
+
+        if(not("title" in jsonData and "description" in jsonData and "difficulty" in jsonData and "score" in jsonData and "tags" in jsonData and "problemcode" in jsonData and "correctoutput" in jsonData)):
+            return JsonResponse({'status':'Required Fields are missing in Create Problem API'}, status=400)
+
+        title = jsonData["title"]
+        description = jsonData["description"]
+        difficulty = jsonData["difficulty"]
+        score = jsonData["score"]
+        tags = jsonData["tags"]
+        correctoutput = jsonData["correctoutput"]
+        
+        problemcode = jsonData["problemcode"]
+
+        newproblem = Problem.objects.create(title=title, description=description, difficulty=difficulty, score=score, problemcode=problemcode, correctoutput=correctoutput)
+
+        if newproblem.pk is not None:
+            return JsonResponse({'status':'Problem Created Successfully'}, status=200)
+        else:
+          return JsonResponse({'status':'Problem Creation Failed from create method'}, status=400)
+        
+    except Exception as e: 
+
+        return JsonResponse({'status':'Create Problem EXCEPTION OCCURED', 'exception':str(e)}, status=400)
